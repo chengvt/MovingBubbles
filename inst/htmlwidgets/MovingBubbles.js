86029deb104707b420064c69b4aa351a47e5d6be
chengvt <vipavee.tri@gmail.com>
2018-08-23 15:21:11 +0900

    make it works on Rstudio Windows and several minor changes

--- inst/htmlwidgets/MovingBubbles.js
+++ inst/htmlwidgets/MovingBubbles.js
@@ -1,93 +1,3 @@
-function adjust_charge_strength(width, height, leaves){
-  let radius_array = d3.values(leaves).map(d => d.r);
-  let small_bubble_count = radius_array.filter(r => r < 20).length;
-  let initial_strength = 2 + Math.pow(1/Math.exp(1), small_bubble_count) * 10;
-  return initial_strength + Math.round(Math.min(width, height) / 150);
-}
-
-function get_leaves(dat, width, height, height_offset){
-  return d3.pack().size([width, height - height_offset]).padding(5)(
-    d3.hierarchy({children: dat})
-    .sum(d => d.value)
-  ).leaves();
-}
-
-function calculate_area_to_value_ratio(leaves, bubble_size){
-  return Math.pow(leaves[0].r,2) / leaves[0].value * bubble_size;
-}
-
-function update_leaves(leaves, dat, frames, j, area_to_value_ratio){
-  for(let i = 0; i < leaves.length; i++){
-    let result = dat.filter(d => d.frame == frames[j]).filter(d => d.key == leaves[i].data.key)
-    leaves[i].r = (result.length !== 0) ? Math.sqrt(result[0].value*area_to_value_ratio) : 0;
-    leaves[i].data.value = (result.length !== 0) ? result[0].value : 0;
-  }
-  return leaves;
-}
-
-function update_frame(j, leaves, dat, frames, area_to_value_ratio, circles, labels, 
-  force, font_size, speed_factor){
-
-  // update leaves to next j
-  leaves = update_leaves(leaves, dat, frames, j, area_to_value_ratio);
-
-  // update force strength when all bubbles are small
-  let leaves_x = d3.values(leaves).map(d => d.x).map(function (num, idx) {
-    return {
-      xmin: num - d3.values(leaves).map(d => d.r)[idx],
-      xmax: num + d3.values(leaves).map(d => d.r)[idx]
-    }
-  });
-  let leaves_y = d3.values(leaves).map(d => d.y).map(function (num, idx) {
-    return {
-      ymin: num - d3.values(leaves).map(d => d.r)[idx],
-      ymax: num + d3.values(leaves).map(d => d.r)[idx]
-    }
-  });
-  let pack_width = d3.max(d3.values(leaves_x).map(d => d.xmax)) - d3.min(d3.values(leaves_x).map(d => d.xmin));
-  let pack_height = d3.max(d3.values(leaves_y).map(d => d.ymax)) - d3.min(d3.values(leaves_y).map(d => d.ymin));
-  force.force("charge", d3.forceManyBody().strength(adjust_charge_strength(pack_width, pack_height, leaves)));
-
-  // transition setting for circles and labels
-  let t = d3.transition().duration(Math.round( 1000 * speed_factor))
-    .ease(d3.easeLinear).tween('update', function(d) {
-      return function(t) { 
-        force.nodes(leaves); 
-        force.alphaTarget(0.3)
-          .restart(); 
-        };
-    })
-
-  // transition circles and labels
-  circles.transition(t).attr("r", d => d.r.toFixed(1));
-  labels.transition(t)
-    .attr("font-size", function(d) { return Math.round(2 * d.r * 0.2 * font_size) + "px"; });
-  
-  // transition frames and tooltips
-  setTimeout(() => {
-    // frame
-    d3.select("p#frame").text(frames[j]), Math.round(1000 * speed_factor);
-  });
-}
-
-function hex_to_rgb(hex){
-  hex = (hex.length = 9) ? hex.substring(0,7) : hex;
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-}
-
-function get_font_color(hex){
-  let rbg = hex_to_rgb(hex);
-  // https://stackoverflow.com/questions/1855884/
-  // Counting the perceptive luminance - human eye favors green color
-  let luminance = 1 - ( 0.299 * rbg.r + 0.587 * rbg.g + 0.114 * rbg.b ) / 255
-  return luminance < 0.5 ? "#303030" : "#dfdfdf";
-}
-
 HTMLWidgets.widget({
 
   name: 'MovingBubbles',
@@ -118,15 +28,15 @@ HTMLWidgets.widget({
       renderValue: function(opts) {
 
         // get data from R
-        let dat = HTMLWidgets.dataframeToD3(opts[0]);
-        let frames = opts[1];
+        var dat = HTMLWidgets.dataframeToD3(opts[0]);
+        var frames = opts[1];
         starting_dat = HTMLWidgets.dataframeToD3(opts[2]);
         bubble_size = opts[3];
-        let font_size = opts[4];
-        let speed_factor = opts[5];
-        let title_size = opts[6];
+        var font_size = opts[4];
+        var speed_factor = opts[5];
+        var title_size = opts[6];
         height_offset = opts[7];
-        let main_title = opts[8];
+        var main_title = opts[8];
 
         // create svg
         d3.select(el).select("svg")
@@ -134,6 +44,7 @@ HTMLWidgets.widget({
 
         // calculate leaves
         leaves = get_leaves(starting_dat, width, height, height_offset);
+        // starting_dat = HTMLWidgets.dataframeToD3(opts[2]);
 
         // calculate area_to_value_ratio
         area_to_value_ratio = calculate_area_to_value_ratio(leaves, bubble_size);
@@ -163,15 +74,15 @@ HTMLWidgets.widget({
           .text(frames[0]);
         
         // add tooltips
-        let tooltip = d3.select(el).append("div")
+        var tooltip = d3.select(el).append("div")
           .attr("id", "tooltip")
-          .attr("class", "hidden")
+          .attr("class", "hidden");
         tooltip.append("p")
           .attr("id", "key")
-          .text("key")
+          .text("key");
         tooltip.append("p")
           .attr("id", "value")
-          .text("value")
+          .text("value");
 
         function mouseover(d) {
           tooltip
@@ -198,14 +109,14 @@ HTMLWidgets.widget({
         }
 
         // add bubbles
-        let circles = svg.selectAll("circle")
+        var circles = svg.selectAll("circle")
           .data(leaves)
           .enter().append("circle")
-            .attr("cx", d => d.x.toFixed(1))
-            .attr("cy", d => d.y.toFixed(1))
-            .attr("r", d => d.r.toFixed(1))
-            .attr("id", d => d.data.key)
-            .style("fill", d => d.data.color)
+            .attr("cx", function(d) {return d.x.toFixed(1);})
+            .attr("cy", function(d) {return d.y.toFixed(1);})
+            .attr("r", function(d) {return d.r.toFixed(1);})
+            .attr("id", function(d) {return d.data.key})
+            .style("fill", function(d) {return d.data.color})
             .each(function(d){
               // add dynamic r getter
               var n = d3.select(this);
@@ -213,47 +124,47 @@ HTMLWidgets.widget({
                 return +n.attr("r");
                 }})
               })
-            .on("mouseover", d => mouseover(d))
-            .on("mouseout", () => {tooltip.classed("hidden", true);})
-            .on("mousemove", d => mousemove(d));
+            .on("mouseover", function(d) {mouseover(d)})
+            .on("mouseout", function() {tooltip.classed("hidden", true);})
+            .on("mousemove", function(d) {mousemove(d)});
         
         // add text inside bubbles
         var labels = svg.selectAll("text")
         .data(leaves)
         .enter().append("text")
-          .text(d => d.data.key)
-          .attr("x", d => d.x.toFixed(1))
-          .attr("y", d => d.y.toFixed(1))
-          .attr("font-size", function(d) { return Math.round(2 * d.r * 0.2 * font_size) + "px" })
+          .text(function(d) {return d.data.key})
+          .attr("x", function(d) {return d.x.toFixed(1)})
+          .attr("y", function(d) {return d.y.toFixed(1)})
+          .attr("font-size", function(d) {return Math.round(2 * d.r * 0.2 * font_size) + "px" })
           .attr("font-family", "sans-serif")
-          .attr("fill", d => get_font_color(d.data.color))
+          .attr("fill", function(d) {return get_font_color(d.data.color);})
           .attr("text-anchor", "middle")
           .attr("alignment-baseline", "middle")
-          .on("mouseover", d => mouseover(d))
-          .on("mouseout", () => {tooltip.classed("hidden", true);})
-          .on("mousemove", d => mousemove(d));
+          .on("mouseover", function(d) {mouseover(d);})
+          .on("mouseout", function() {tooltip.classed("hidden", true);})
+          .on("mousemove", function(d) {mousemove(d);});
         
         force = d3.forceSimulation(leaves)
           .force("charge", d3.forceManyBody().strength(adjust_charge_strength(width, height - height_offset, leaves)))
           .force("center", d3.forceCenter(width / 2, height / 2))
-          .force("collide", d3.forceCollide().radius(d => d.rt + 5).strength(0.7))
+          .force("collide", d3.forceCollide().radius(function(d) {return (d.rt + 5)}).strength(0.7).iterations(3))
           .on("tick", function(e) {
             svg.selectAll("circle")
-              .attr("cx", d => d.x.toFixed(1))
-              .attr("cy", d => d.y.toFixed(1));
+              .attr("cx", function(d) {return d.x.toFixed(1)})
+              .attr("cy", function(d) {return d.y.toFixed(1)});
             svg.selectAll("text")
-              .attr("x", d => d.x.toFixed(1))
-              .attr("y", d => d.y.toFixed(1));
+              .attr("x", function(d) {return d.x.toFixed(1)})
+              .attr("y", function(d) {return d.y.toFixed(1)});
             })
           .stop();
         
         // initialize simulation
-        for (let i = 0; i < 300; ++i) { force.tick(); }
+        for (var i = 0; i < 300; ++i) { force.tick(); }
         force.alphaTarget(0.3)
           .restart();
 
         // change frame at interval
-        let j = 1;
+        var j = 1;
         d3.interval(function() {
           update_frame(j, leaves, dat, frames, area_to_value_ratio, circles, labels, 
             force, font_size, speed_factor);
@@ -269,11 +180,12 @@ HTMLWidgets.widget({
         svg.attr("width", width)
           .attr("height", height - height_offset);
 
-        // update frame size
+        // update frame and main title size
         d3.select("p#frame").style("width", width + "px");
+        d3.select("p#main").style("width", width + "px");
 
         // recalculate area_to_value_ratio
-        let leaves_tmp = get_leaves(starting_dat, width, height, height_offset);
+        var leaves_tmp = get_leaves(starting_dat, width, height, height_offset);
         area_to_value_ratio = calculate_area_to_value_ratio(leaves_tmp, bubble_size);
 
         // update force
@@ -283,3 +195,94 @@ HTMLWidgets.widget({
     }
   }
 });
+
+function adjust_charge_strength(width, height, leaves){
+  var radius_array = d3.values(leaves).map(function(d) {return d.r});
+  var small_bubble_count = radius_array.filter(function(r) {return (r < 10 && r > 1)}).length;
+  var initial_strength = 2 + Math.pow(1/Math.exp(1), small_bubble_count) * 10;
+  return initial_strength + Math.round(Math.min(width, height) / 200);
+}
+
+function get_leaves(dat, width, height, height_offset){
+  return d3.pack().size([width, height - height_offset]).padding(5)(
+    d3.hierarchy({children: dat})
+    .sum(function(d) {return d.value})
+  ).leaves();
+}
+
+function calculate_area_to_value_ratio(leaves, bubble_size){
+  return Math.pow(leaves[0].r,2) / leaves[0].value * bubble_size;
+}
+
+function update_leaves(leaves, dat, frames, j, area_to_value_ratio){
+  for(var i = 0; i < leaves.length; i++){
+    var result = dat.filter(function(d) {return (d.frame == frames[j])})
+      .filter(function(d) {return (d.key == leaves[i].data.key)});
+    leaves[i].r = (result.length !== 0) ? Math.sqrt(result[0].value*area_to_value_ratio) : 0;
+    leaves[i].data.value = (result.length !== 0) ? result[0].value : 0;
+  }
+  return leaves;
+}
+
+function update_frame(j, leaves, dat, frames, area_to_value_ratio, circles, labels, 
+  force, font_size, speed_factor){
+
+  // update leaves to next j
+  leaves = update_leaves(leaves, dat, frames, j, area_to_value_ratio);
+
+  // update force strength when all bubbles are small
+  var leaves_x = d3.values(leaves).map(function(d) {return d.x}).map(function (num, idx) {
+    return {
+      xmin: num - d3.values(leaves).map(function(d) {return d.r})[idx],
+      xmax: num + d3.values(leaves).map(function(d) {return d.r})[idx]
+    }
+  });
+  var leaves_y = d3.values(leaves).map(function(d) {return d.y}).map(function (num, idx) {
+    return {
+      ymin: num - d3.values(leaves).map(function(d) {return d.r})[idx],
+      ymax: num + d3.values(leaves).map(function(d) {return d.r})[idx]
+    }
+  });
+  var pack_width = d3.max(d3.values(leaves_x).map(function(d) {return d.xmax})) - d3.min(d3.values(leaves_x).map(function(d) {return d.xmin}));
+  var pack_height = d3.max(d3.values(leaves_y).map(function(d) {return d.ymax})) - d3.min(d3.values(leaves_y).map(function(d) {return d.ymin}));
+  force.force("charge", d3.forceManyBody().strength(adjust_charge_strength(pack_width, pack_height, leaves)));
+
+  // transition setting for circles and labels
+  var t = d3.transition().duration(Math.round( 1000 * speed_factor))
+    .ease(d3.easeLinear).tween('update', function(d) {
+      return function(t) { 
+        force.nodes(leaves); 
+        force.alphaTarget(0.3)
+          .restart(); 
+        };
+    })
+
+  // transition circles and labels
+  circles.transition(t).attr("r", function(d) {return d.r.toFixed(1)});
+  labels.transition(t)
+    .attr("font-size", function(d) { return Math.round(2 * d.r * 0.2 * font_size) + "px"; });
+  
+  // transition frames and tooltips
+  setTimeout(function() {
+    // frame
+    d3.select("p#frame").text(frames[j]), Math.round(1000 * speed_factor);
+  });
+}
+
+function hex_to_rgb(hex){
+  hex = (hex.length = 9) ? hex.substring(0,7) : hex;
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function get_font_color(hex){
+  var rbg = hex_to_rgb(hex);
+  // https://stackoverflow.com/questions/1855884/
+  // Counting the perceptive luminance - human eye favors green color
+  var luminance = 1 - ( 0.299 * rbg.r + 0.587 * rbg.g + 0.114 * rbg.b ) / 255
+  return luminance < 0.5 ? "#303030" : "#dfdfdf";
+}
